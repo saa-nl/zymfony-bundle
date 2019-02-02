a6591a3b59812bf2751ab0e617aa11073e9a02ea
Deniz Zoeteman <dzoeteman@saa.nl>
2019-02-02 15:30:17 +0100

    Initial bundle setup

---
+++ src/DependencyInjection/ZymfonyExtension.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace SAA\ZymfonyBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class ZymfonyExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+
+    }
+}
---
+++ src/ZymfonyBundle.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace SAA\ZymfonyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ZymfonyBundle extends Bundle
+{
+
+}
